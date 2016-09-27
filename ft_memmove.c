2effb9e2a43ae08bb23f86a1b1562ab6aa995a36
Mason Young <masondyoung@gmail.com>
2016-09-27 14:21:32 -0700

    added a comment to ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,50 @@
 /*   By: myoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 12:10:20 by myoung            #+#    #+#             */
-/*   Updated: 2016/09/25 23:11:06 by myoung           ###   ########.fr       */
+/*   Updated: 2016/09/27 13:35:50 by myoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** ft_memmove explained
+**
+** str:
+** 0123456789
+** ^   ^
+** src dst
+**
+** src = &str[0];
+** dst = &str[4];
+**
+** ft_memmove(dst, src, 5);
+**
+** Our goal is to move 01234 (the first 5 bytes in src)
+**  to str[4] which is our dst.
+**
+** Move forwards:
+** 0123456789
+** 0123056789
+** 0123016789
+** 0123012789
+** 0123012389
+** 0123012309
+** ....01230.
+**
+** we destroyed our 4! This isn't what we want!
+**
+** Move Backwards:
+** 0123456789
+** 0123456749
+** 0123456349
+** 0123452349
+** 0123412349
+** 0123012349
+** ....01234.
+**
+** This works!
+*/
 
 void		*ft_memmove(void *dst, const void *src, size_t n)
 {
