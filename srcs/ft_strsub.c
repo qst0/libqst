8f4b15baf6c16290c81ccc3c3c3e43aa9e860dd9
Mason Young <masondyoung@gmail.com>
2016-12-22 07:07:19 -0800

    fixed comments for norm

--- srcs/ft_strsub.c
+++ srcs/ft_strsub.c
@@ -6,11 +6,23 @@
 /*   By: myoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 18:22:32 by myoung            #+#    #+#             */
-/*   Updated: 2016/10/19 15:32:35 by myoung           ###   ########.fr       */
+/*   Updated: 2016/12/22 07:04:52 by myoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
+
+/*
+** if the string is null return null
+** make a new string the size of len, with ft_stnew
+** check and make sure it worked.
+** i = 0
+** iterate while i < len
+**  set the postition i in new to the start + i of the str
+**  itterate i
+**
+**  return the new str you made
+*/
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
